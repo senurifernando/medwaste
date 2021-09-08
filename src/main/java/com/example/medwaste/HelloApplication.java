1b0cd8f5c318bc5e42c05ea4453ffea0e2a80a95
senurifernando <senuri0604@gmail.com>
2021-09-09 00:52:19 +0530

    few changes added

--- src/main/java/com/example/medwaste/HelloApplication.java
+++ src/main/java/com/example/medwaste/HelloApplication.java
@@ -10,14 +10,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 528, 400);
+        stage.setTitle("Welcome to Medical waste Management System ");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
